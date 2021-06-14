8f91d77c750143ec2f0127542cb8ed1e90ea12fe
Rhayssadandara <rhayssadandara98@gmail.com>
2021-06-14 15:00:08 -0300

    primeiro projeto React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  state = {
+    nome: "Dandara",
+    idade: 11,
+    cor: "azul"
+  }
+ handleClick = () => {
+  let nomeAtual = this.state.nome === "Dandara" ? "Rhayssa Dandara" : "Dandara" 
+  this.setState({
+    nome: nomeAtual
+  })
 }
 
-export default App;
+  render() {
+    return(
+      <div>
+       <h1>{this.state.nome}</h1>
+       <button onClick={this.handleClick}>Clica aqui</button>
+      </div>
+    )
+  }
+}
