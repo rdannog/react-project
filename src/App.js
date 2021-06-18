bc9558c7897f3038d7968aaba932001414f60eb0
Rhayssadandara <rhayssadandara98@gmail.com>
2021-06-18 11:43:50 -0300

    estudando métodos de ciclo de vida

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
 import React from "react";
 
-export default class App extends React.Component{
+export default class App extends React.Component {
   state = {
-      posts:[
-        {
-          id: 1,
-          title: "O título",
-          body: "O corpo"
-        },
-        {
-          id: 2,
-          title: "O título 2",
-          body: "O corpo 2"
-        },
-        {
-          id: 3,
-          title: "O título 3",
-          body: "O corpo 3"
-        }
-      ]
-    };
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: "O título 1",
+        body: "Esse é o texto do corpo 1"
+      }, {
+        id: 2,
+        title: "O título 2",
+        body: "Esse é o texto do corpo 2"
+      },{
+        id: 3,
+        title: "O título 3",
+        body: "Esse é o texto do corpo 3"
+      }
+    ]
+  };
+
+  timeoutUpdate = null;
+
+  componentDidMount(){
+    this.handleTimeout();
+  }
+  
+  componentDidUpdate(){
+    this.handleTimeout()
+  }
+
+  componentWillUnmount(){
+    clearTimeout(this.timeoutUpdate)
+  }
+  handleTimeout() {
+    const {posts, counter} = this.state;
+    posts[0].title = "Mudei o título"
+    
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({posts, counter: counter+1})
+    }, 1000)
+  }
 
   render() {
-    const { posts } = this.state
-    return(
+    const { posts, counter } = this.state
+    return (
       <div>
-       {posts.map(posts =>(
-         <div>
-           <h1 key={posts.id}>{posts.title}</h1>
-           <p>{posts.body}</p>
-         </div>
-       ))}
+        <h1>{counter}</h1>
+        {posts.map((posts, index) => (
+          <div key={posts.body}>
+            <h1 key={posts.id}>{posts.title}</h1>
+            <p key={index}>{posts.body}</p>
+          </div>
+        ))}
       </div>
     )
   }
